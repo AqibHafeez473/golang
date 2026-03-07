23d7e324b02ebffebcccd47d8223135c69818674
Aqib Hafeez <aqibhafeez422@gmail.com>
2026-03-07 21:06:31 +0500

    add function lab

---
+++ function/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func simplefunction()  {
+
+
+	println("simple function")
+	
+}
+
+
+func main ()  {
+
+	fmt.Println("we are learning Golang Function")
+	simplefunction()
+}
